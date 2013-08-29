cb3799941f3f07ff7d07d5bb72fc91dddff21391
Lily <qvang.j@gmail.com>
2013-08-28 22:40:22 -0700

    second commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,18 +3,33 @@ get '/' do
 end
 
 get '/create_post' do
-  @category = params["category"]
-  @title = params["title"]
-  @description = params["description"]
-  @price = params["price"]
-  @email = params["email"]
-  post = Post.create(:title => @title, :description => @description, :price => @price,
-    :email => @email)
-  # post.categories.create(:name => @category)
-  # this doean't work :(
   erb :create_post
 end
 
 post '/submitted_form' do
+  cat = params['new_category'].empty? ? params['category'] : params['new_category']
+  @category = Category.where(name: cat).first_or_create
+  @title = params["title"]
+  @description = params["description"]
+  @price = params["price"]
+  @email = params["email"]
+  @post = Post.create(:title => @title, :description => @description, :price => @price,
+    :email => @email, :category => @category)
   erb :submitted_form
 end
+
+get '/post_page' do
+  erb :post_page
+end
+
+get '/:category' do
+  @category = Category.where(name: params[:category]).first
+  @posts = @category.posts
+  erb :posts_in_category
+end
+
+get '/category/:title' do
+  @post = Post.where(title: params[:title]).first
+  erb :post_page
+end
+
--- db/migrate/20130827183736_posts.rb
+++ db/migrate/20130827183736_posts.rb
@@ -1,15 +1,12 @@
 class Posts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.integer :category_id
       t.string :title
       t.string :description 
       t.string :price
       t.string :email
     end
-
-    create_table :categories_posts do |t|
-      t.belongs_to :category
-      t.belongs_to :post
-    end
   end
 end
+ 
